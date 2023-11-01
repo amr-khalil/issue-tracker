c8fab9efffe63ffb0bd695c67fc73f5cc119642e
Amr Khalil <amr.alshemaly93@gmail.com>
2023-11-01 08:37:30 +0100

    disable SSR for MD Editor

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,8 +1,14 @@
+import { Box } from '@radix-ui/themes'
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function LoadingNewIssuePage() {
   return (
-    <div>loading...</div>
+    <Box className='max-w-xl'>
+        < Skeleton height='2rem'/>
+        < Skeleton height='20rem'/>
+    </Box>
   )
 }
 
