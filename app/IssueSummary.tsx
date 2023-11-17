323e2dd38379a360c6ec2157963b5ddc4b3aadbb
Amr Khalil <amr.alshemaly93@gmail.com>
2023-11-17 19:26:02 +0100

    added dashboard

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,32 @@
+import { Card, Flex, Text } from '@radix-ui/themes'
+import React from 'react'
+import Link from 'next/link'
+
+interface Probs {
+    open: number
+    inProgress: number
+    closed: number
+}
+
+function IssueSummary({open, inProgress, closed}:Probs) {
+    const containers = [{label: 'Open Issues', value:open, status:'OPEN'},
+                        {label: 'In-Progress Issues', value:inProgress, status:'IN_PROGRESS'},
+                        {label: 'Closed Issues', value:closed, status:'CLOSED'}]
+
+  return (
+    <Flex gap='4'>
+        {containers.map(container => <Card key={container.label}>
+            <Flex direction="column" gap='1'>
+            <Link 
+            className='text-sm font-medium'
+            href={`/issues/?status=${container.status}`}>{container.label}</Link>
+            <Text size='5' className='font-bold'>
+                {container.value}
+            </Text>
+            </Flex>
+        </Card>)}
+    </Flex>
+  )
+}
+
+export default IssueSummary
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 import Image from 'next/image'
+import LatestIssues from './LatestIssues'
+import IssueSummary from './IssueSummary'
+import prisma from '@/prisma/client'
+import IssueChart from './IssueChart'
+import { Flex, Grid } from '@radix-ui/themes'
 
-export default function Home() {
+export default async function  Home() {
+  const open = await prisma.issue.count({where:{status:'OPEN'}})
+  const inProgress = await prisma.issue.count({where:{status:'inProgress'}})
+  const closed = await prisma.issue.count({where:{status:'CLOSED'}})
   return (
-    <div>Hello World</div>
+    <Grid columns={{initial:'1', md:'2'}} gap='5'>
+      <Flex direction='column' gap='5'>
+      <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
+      <IssueChart open={open} inProgress={inProgress} closed={closed}/>
+      </Flex>
+      
+       <LatestIssues />
+      
+    </Grid>
+    
+  
   )
 }
